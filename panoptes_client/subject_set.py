0d9b348e055d5215b9a7993db97029d3e3692e2f
Adam McMaster <adam@zooniverse.org>
2016-06-15 21:22:32 +0100

    Add SubjectSet._edit_attributes

--- panoptes_client/subject_set.py
+++ panoptes_client/subject_set.py
@@ -3,5 +3,18 @@ from panoptes_client.panoptes import PanoptesObject, LinkResolver
 class SubjectSet(PanoptesObject):
     _api_slug = 'subject_sets'
     _link_slug = 'subject_sets'
+    _edit_attributes = (
+        'display_name',
+        {
+            'links': (
+                'subjects',
+                'project',
+                'workflows',
+            ),
+            'metadata': (
+                'category',
+            )
+        },
+    )
 
 LinkResolver.register(SubjectSet)
