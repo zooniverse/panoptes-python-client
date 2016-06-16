8567ecc03142a63d84718c72e74612c1986fac04
Adam McMaster <adam@zooniverse.org>
2016-06-16 15:26:15 +0100

    Set version to 0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='panoptes_client',
-    version='0.1-pre',
+    version='0.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
