7e0457f70413238113ad3cae17efd15a81bb368e
Adam McMaster <adam@zooniverse.org>
2016-07-06 15:32:43 +0100

    Add classifications export

--- panoptes_client/project.py
+++ panoptes_client/project.py
@@ -1,4 +1,10 @@
-from panoptes_client.panoptes import PanoptesObject, LinkResolver
+import datetime
+import requests
+import time
+
+from panoptes_client.panoptes import (
+    LinkResolver, PanoptesAPIException, PanoptesObject
+)
 
 class Project(PanoptesObject):
     _api_slug = 'projects'
@@ -17,5 +23,59 @@ class Project(PanoptesObject):
         if not id and not slug:
             return None
         return cls.where(id=id, slug=slug).next()
+
+    def get_classifications_export(
+        self,
+        generate=False,
+        wait=False,
+        wait_timeout=60
+    ):
+        if generate:
+            self.generate_classifications_export()
+
+        if generate or wait:
+            export = self.wait_classifications_export(wait_timeout)
+        else:
+            export = self.describe_classifications_export()
+
+        return requests.get(
+            export['media'][0]['src'],
+            stream=True
+        )
+
+    def wait_classifications_export(self, timeout=60):
+        success = False
+        end_time = datetime.datetime.now() + datetime.timedelta(
+            seconds=timeout
+        )
+
+        while datetime.datetime.now() < end_time:
+            export_description = self.describe_classifications_export()
+            export_metadata = export_description['media'][0]['metadata']
+            if export_metadata.get('state', '') == 'ready':
+                success = True
+                break
+            time.sleep(2)
+
+        if not success:
+            raise PanoptesAPIException(
+                'classifications_export not ready within {} seconds'.format(
+                    timeout
+                )
+            )
+
+        return export_description
+
+    def generate_classifications_export(self):
+        return Project.post(
+            self._classifications_export_path(),
+            json = {"media":{"content_type":"text/csv"}}
+        )[0]
+
+    def describe_classifications_export(self):
+        return Project.get(self._classifications_export_path())[0]
+
+    def _classifications_export_path(self):
+        return '{}/classifications_export'.format(self.id)
 
 LinkResolver.register(Project)
