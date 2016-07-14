a7dc35c4ed92ad701da2a5a01401be7cc858828a
srallen <srallen@users.noreply.github.com>
2016-07-14 13:10:33 -0500

    Add ProjectPreferences (#9)
    * Add ProjectPreferences
    
    * Addressing hound

--- panoptes_client/__init__.py
+++ panoptes_client/__init__.py
@@ -3,3 +3,4 @@ from panoptes_client.project import Project
 from panoptes_client.subject import Subject
 from panoptes_client.subject_set import SubjectSet
 from panoptes_client.workflow import Workflow
+from panoptes_client.project_preferences import ProjectPreferences
---
+++ panoptes_client/project_preferences.py
@@ -0,0 +1,15 @@
+from panoptes_client.panoptes import PanoptesObject, LinkResolver
+
+
+class ProjectPreferences(PanoptesObject):
+    _api_slug = 'project_preferences'
+    _link_slug = 'project_preferences'
+    _edit_attributes = (
+        'preferences',
+    )
+
+    @classmethod
+    def find(cls, user_id, project_id):
+        return cls.where(user_id=user_id, project_id=project_id).next()
+
+LinkResolver.register(ProjectPreferences)
