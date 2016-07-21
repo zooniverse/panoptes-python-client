b42f72e1c47111abbb32d96bb5e53a3125409d9e
Sarah Allen <sarah@zooniverse.org>
2016-07-21 14:07:36 -0500

    Added User model. Find ProjectPreferences by User and Project object.

--- panoptes_client/__init__.py
+++ panoptes_client/__init__.py
@@ -5,3 +5,4 @@ from panoptes_client.subject_set import SubjectSet
 from panoptes_client.workflow import Workflow
 from panoptes_client.classification import Classification
 from panoptes_client.project_preferences import ProjectPreferences
+from panoptes_client.user import User
