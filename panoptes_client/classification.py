23fc2bbd22aa8d45301207b0608634df4414707f
Adam McMaster <adam@zooniverse.org>
2016-07-27 14:28:36 +0100

    Add scope kwarg to Classification.where()

--- panoptes_client/classification.py
+++ panoptes_client/classification.py
@@ -1,8 +1,16 @@
 from panoptes_client.panoptes import LinkResolver, PanoptesObject
 
+
 class Classification(PanoptesObject):
     _api_slug = 'classifications'
     _link_slug = 'classification'
     _edit_attributes = ( )
+
+    @classmethod
+    def where(cls, **kwargs):
+        scope = kwargs.pop('scope', None)
+        if not scope:
+            return super(Classification, cls).where(**kwargs)
+        return cls.paginated_results(*cls.get(scope, params=kwargs))
 
 LinkResolver.register(Classification)
