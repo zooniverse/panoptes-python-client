5dbdfff832199f926f34c8dea002d304d9426e26
Adam McMaster <adam@zooniverse.org>
2016-08-04 15:37:27 +0100

    Generic support for all Panoptes exports
    Talk exports still not supported. They'll come separately.

--- panoptes_client/project.py
+++ panoptes_client/project.py
@@ -6,6 +6,12 @@ from panoptes_client.panoptes import (
     LinkResolver, PanoptesAPIException, PanoptesObject
 )
 
+TALK_EXPORT_TYPES = (
+    'talk_comments',
+    'talk_tags',
+)
+
+
 class Project(PanoptesObject):
     _api_slug = 'projects'
     _link_slug = 'project'
@@ -24,33 +30,45 @@ class Project(PanoptesObject):
             return None
         return cls.where(id=id, slug=slug).next()
 
-    def get_classifications_export(
+    def get_export(
         self,
+        export_type,
         generate=False,
         wait=False,
         wait_timeout=60
     ):
+        if export_type in TALK_EXPORT_TYPES:
+            return self._get_talk_export(
+                export_type,
+                generate,
+                wait,
+                wait_timeout
+            )
+
         if generate:
-            self.generate_classifications_export()
+            self.generate_export(export_type)
 
         if generate or wait:
-            export = self.wait_classifications_export(wait_timeout)
+            export = self.wait_export(export_type, wait_timeout)
         else:
-            export = self.describe_classifications_export()
+            export = self.describe_export(export_type)
 
         return requests.get(
             export['media'][0]['src'],
             stream=True
         )
 
-    def wait_classifications_export(self, timeout=60):
+    def wait_export(self, export_type, timeout=60):
+        if export_type in TALK_EXPORT_TYPES:
+            return self._wait_talk_export(export_type, timeout)
+
         success = False
         end_time = datetime.datetime.now() + datetime.timedelta(
             seconds=timeout
         )
 
         while datetime.datetime.now() < end_time:
-            export_description = self.describe_classifications_export()
+            export_description = self.describe_export(export_type)
             if export_description:
                 export_metadata = export_description['media'][0]['metadata']
                 if export_metadata.get('state', '') == 'ready':
@@ -60,23 +78,48 @@ class Project(PanoptesObject):
 
         if not success:
             raise PanoptesAPIException(
-                'classifications_export not ready within {} seconds'.format(
+                '{}_export not ready within {} seconds'.format(
+                    export_type,
                     timeout
                 )
             )
 
         return export_description
 
-    def generate_classifications_export(self):
+    def generate_export(self, export_type):
+        if export_type in TALK_EXPORT_TYPES:
+            return self._generate_talk_export(export_type)
+
         return Project.post(
-            self._classifications_export_path(),
+            self._export_path(export_type),
             json = {"media":{"content_type":"text/csv"}}
         )[0]
 
-    def describe_classifications_export(self):
-        return Project.get(self._classifications_export_path())[0]
+    def describe_export(self, export_type):
+        if export_type in TALK_EXPORT_TYPES:
+            return self._describe_talk_export(export_type)
 
-    def _classifications_export_path(self):
-        return '{}/classifications_export'.format(self.id)
+        return Project.get(self._export_path(export_type))[0]
+
+    def _get_talk_export(
+        self,
+        export_type,
+        generate,
+        wait,
+        wait_timeout
+    ):
+        raise NotImplementedError('Talk exports are not yet supported')
+
+    def _wait_talk_export(self, export_type, timeout):
+        raise NotImplementedError('Talk exports are not yet supported')
+
+    def _generate_talk_export(self, export_type):
+        raise NotImplementedError('Talk exports are not yet supported')
+
+    def _describe_talk_export(self, export_type):
+        raise NotImplementedError('Talk exports are not yet supported')
+
+    def _export_path(self, export_type):
+        return '{}/{}_export'.format(export_type, self.id)
 
 LinkResolver.register(Project)
