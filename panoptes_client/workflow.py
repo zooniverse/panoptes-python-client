ffd1cb73350c21a28cd284f372115773eb952030
Adam McMaster <adam@zooniverse.org>
2016-08-10 10:50:22 +0100

    Add Workflow->SubjectSet linking
    Closes #2

--- panoptes_client/workflow.py
+++ panoptes_client/workflow.py
@@ -1,5 +1,7 @@
 from panoptes_client.panoptes import PanoptesObject, LinkResolver
 from panoptes_client.subject import Subject
+from panoptes_client.subject_set import SubjectSet
+
 
 class Workflow(PanoptesObject):
     _api_slug = 'workflows'
@@ -18,5 +20,46 @@ class Workflow(PanoptesObject):
                 'retirement_reason': reason
             }
         )
+
+    def add_subject_sets(self, subject_sets, batch_size=100):
+        _subject_sets = self._build_subject_set_list(subject_sets)
+
+        for _subject_sets_batch in [
+            _subject_sets[i:i+batch_size]
+            for i in xrange(0, len(_subject_sets), batch_size)
+        ]:
+            self.post(
+                '{}/links/subject_sets'.format(self.id),
+                json={'subject_sets': _subject_sets_batch}
+            )
+
+    def remove_subject_sets(self, subject_sets):
+        _subject_sets = self._build_subject_set_list(subject_sets)
+        _subject_set_ids = ",".join(_subject_sets)
+
+        self.delete(
+            '{}/links/subject_sets/{}'.format(self.id, _subject_set_ids)
+        )
+
+    def _build_subject_set_list(self, subject_sets):
+        if not type(subject_sets) in (tuple, list):
+            subject_sets = [subject_sets]
+
+        _subject_sets = []
+        for subject_set in subject_sets:
+            if not (
+                isinstance(subject_set, SubjectSet)
+                or isinstance(subject_set, (int, str, unicode,))
+            ):
+                raise TypeError
+
+            if isinstance(subject_set, SubjectSet):
+                _subject_set_id = subject_set.id
+            else:
+                _subject_set_id = str(subject_set)
+
+            _subject_sets.append(_subject_set_id)
+
+        return _subject_sets
 
 LinkResolver.register(Workflow)
