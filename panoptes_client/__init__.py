338884db86811d24370409be487e072979b31fb1
Sarah Allen <sarah@zooniverse.org>
2016-08-26 09:04:25 -0500

    Remove SetMemberSubject from init

--- panoptes_client/__init__.py
+++ panoptes_client/__init__.py
@@ -6,4 +6,3 @@ from panoptes_client.workflow import Workflow
 from panoptes_client.classification import Classification
 from panoptes_client.project_preferences import ProjectPreferences
 from panoptes_client.user import User
-from panoptes_client.set_member_subject import SetMemberSubject
