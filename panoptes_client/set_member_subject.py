5b9072a4483b1fc26761d65ed563842b2aaf8a89
srallen <srallen@users.noreply.github.com>
2016-08-30 04:44:33 -0500

    Removed subject_sets method and SetMemberSubject (#46)
    * Removed subject_sets method and SetMemberSubject
    
    * Remove SetMemberSubject from init

--- panoptes_client/set_member_subject.py
+++
@@ -1,9 +0,0 @@
-from panoptes_client.panoptes import PanoptesObject, LinkResolver
-
-
-class SetMemberSubject(PanoptesObject):
-    _api_slug = 'set_member_subjects'
-    _link_slug = 'set_member_subjects'
-    _edit_attributes = ()
-
-LinkResolver.register(SetMemberSubject)
