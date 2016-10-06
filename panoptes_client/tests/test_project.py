d00eb395189def65e8371daf944804518da41947
Adam McMaster <adam@12mtns.com>
2016-10-06 14:56:53 -0500

    Set up unit tests (#52)

---
+++ panoptes_client/tests/test_project.py
@@ -0,0 +1,21 @@
+import unittest
+
+from panoptes_client import Project
+
+
+class TestProject(unittest.TestCase):
+    def test_find_id(self):
+        p = Project.find(1)
+        self.assertEqual(p.id, '1')
+
+    def test_find_slug(self):
+        p = Project.find(slug='zooniverse/snapshot-supernova')
+        self.assertEqual(p.id, '1')
+
+    def test_find_unknown_id(self):
+        p = Project.find(0)
+        self.assertEqual(p, None)
+
+    def test_find_unknown_slug(self):
+        with self.assertRaises(StopIteration):
+            Project.find(slug='invalid_slug')
