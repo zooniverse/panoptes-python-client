957cc039cb7fee235c4072295a66ce81d4bf9159
Campbell Allen <campbell.allen@gmail.com>
2016-12-01 11:20:45 +0000

    add SetMemberSubject resource
    allow the raw link params to be exposed for fast index lookup on sets / subjects

---
+++ panoptes_client/tests/test_set_member_subject.py
@@ -0,0 +1,17 @@
+import unittest
+
+from panoptes_client import SetMemberSubject
+from panoptes_client.panoptes import PanoptesAPIException
+
+class TestSetMemberSubject(unittest.TestCase):
+    def test_find_id(self):
+        sms = SetMemberSubject.find(1000)
+        self.assertEqual(sms.id, '1000')
+
+    def test_subject_set_id(self):
+        sms = SetMemberSubject.find(1000)
+        self.assertEqual(sms.subject_set_id(), '2')
+
+    def test_subject_id(self):
+        sms = SetMemberSubject.find(1000)
+        self.assertEqual(sms.subject_id(), '1458')
