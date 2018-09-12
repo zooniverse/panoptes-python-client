64a79519204e491e369508e530097b8cb23b7967
Campbell Allen <campbell.allen@gmail.com>
2018-09-12 19:21:37 +0100

    add project avatar resource links

---
+++ panoptes_client/avatar.py
@@ -0,0 +1,30 @@
+from panoptes_client.panoptes import (
+    Panoptes,
+    PanoptesAPIException,
+    PanoptesObject,
+    LinkResolver,
+)
+from panoptes_client.project import Project
+
+class Avatar(PanoptesObject):
+    _api_slug = 'avatar'
+    _link_slug = 'avatars'
+    _edit_attributes = ()
+
+    @classmethod
+    def http_get(cls, path, params={}, headers={}):
+        project = params.pop('project')
+        # print()
+        # print(Project.url(project.id))
+        # print()
+
+        avatar_response = Panoptes.client().get(
+            Project.url(project.id) + cls.url(path),
+            params,
+            headers,
+        )
+        print avatar_response.raw
+        return avatar_response
+
+LinkResolver.register(Avatar)
+LinkResolver.register(Avatar, 'avatar')
