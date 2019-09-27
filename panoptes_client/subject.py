b0e35783a827cfbc27ad2447fb233f621572b764
Hugh Dickinson <hughdickinson@users.noreply.github.com>
2019-09-27 08:52:53 +0100

    Removed superfluous module qualifier

--- panoptes_client/subject.py
+++ panoptes_client/subject.py
@@ -209,7 +209,7 @@ class Subject(PanoptesObject):
     def set_raw(self, raw, etag=None, loaded=True):
         super(Subject, self).set_raw(raw, etag, loaded)
         if loaded and self.metadata:
-            self._original_metadata = copy.deepcopy(self.metadata)
+            self._original_metadata = deepcopy(self.metadata)
         elif loaded:
             self._original_metadata = None
 
