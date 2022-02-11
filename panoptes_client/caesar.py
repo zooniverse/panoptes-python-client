c69ff539f1451eafaf134199b9750f189e7df6bd
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2022-02-11 13:49:01 -0600

    removing whitespaces from hound sniffs

--- panoptes_client/caesar.py
+++ panoptes_client/caesar.py
@@ -1,4 +1,3 @@
-from multiprocessing.sharedctypes import Value
 from panoptes_client.panoptes import Panoptes
 
 
@@ -10,7 +9,7 @@ class Caesar(object):
         'user': ['promote_user']
     }
 
-    def __init__(self, endpoint =  'https://caesar-staging.zooniverse.org', redirect_url= 'https://caesar.zooniverse.org/auth/zooniverse/callback'):
+    def __init__(self, endpoint='https://caesar-staging.zooniverse.org', redirect_url='https://caesar.zooniverse.org/auth/zooniverse/callback'):
         self.endpoint = endpoint
         self.headers = {
             'Accept': 'application/json'
@@ -39,22 +38,22 @@ class Caesar(object):
 
     def get_reductions_by_workflow_and_subject(self, workflow_id, subject_id):
         return self.http_get(f'workflows/{workflow_id}/subjects/{subject_id}/reductions')
-    
+
     def get_workflow_extractors(self, workflow_id):
         return self.http_get(f'workflows/{workflow_id}/extractors')
-    
+
     def get_workflow_reducers(self, workflow_id):
         return self.http_get(f'workflows/{workflow_id}/reducers')
- 
+
     def get_extracts_by_workflow_and_subject(self, workflow_id, subject_id):
-        return self.http_get(f'workflows/{workflow_id}/extractors/extractor/extracts', params={'subject_id' : subject_id})
+        return self.http_get(f'workflows/{workflow_id}/extractors/extractor/extracts', params={'subject_id': subject_id})
 
     def get_extractor_by_workflow_and_id(self, workflow_id, extractor_id):
         return self.http_get(f'workflows/{workflow_id}/extractors/{extractor_id}')
 
     def add_workflow(self, workflow_id):
         return self.http_post('workflows', json={'workflow': {'id': workflow_id}})
-    
+
     def create_workflow_extractor(self, workflow_id, extractor_key, extractor_type,task_key='T0', other_extractor_attributes={}):
         self.validate_extractor_type(extractor_type)
         payload = {
@@ -66,7 +65,7 @@ class Caesar(object):
             }
         }
         return self.http_post(f'workflows/{workflow_id}/extractors', json=payload )
-    
+
     def create_workflow_reducer(self, workflow_id, reducer_type, key, other_reducer_attributes={}):
         self.validate_reducer_type(reducer_type)
         payload = {
@@ -76,19 +75,19 @@ class Caesar(object):
                 **other_reducer_attributes
             }
         }
-        
+
         return self.http_post(f'workflows/{workflow_id}/reducers', json=payload)
 
-    def create_workflow_rule(self, workflow_id, rule_type,condition_string='[]'):
+    def create_workflow_rule(self, workflow_id, rule_type, condition_string='[]'):
         self.validate_rule_type(rule_type)
 
-        #condition string e.g.'["gte", ["lookup", "count.classifications", 0], ["const", 30]]'
+        # condition string e.g.'["gte", ["lookup", "count.classifications", 0], ["const", 30]]'
         payload={f'{rule_type}_rule': {
                 'condition_string': condition_string
             }
         }
         return self.http_post(f'workflows/{workflow_id}/{rule_type}_rules', json=payload)
-    
+
     def create_workflow_rule_effect(self, workflow_id, rule_type, rule_id, action, config={}):
         self.validate_rule_type(rule_type)
         self.validate_action(action)
@@ -101,19 +100,19 @@ class Caesar(object):
 
         request_url = f'workflows/{workflow_id}/{rule_type}_rules/{rule_id}/{rule_type}_rule_effects'
         return self.http_post(request_url, json=payload)
-        
+
     def validate_rule_type(self, rule_type):
         if rule_type not in self.RULE_TO_ACTION_TYPES.keys():
             raise ValueError(f'Invalid rule type: {rule_type}.  Rule types can either be by "subject" or "user" ')
-    
+
     def validate_reducer_type(self, reducer_type):
         if reducer_type not in self.REDUCER_TYPES:
             raise ValueError('Invalid reducer type')
-    
+
     def validate_extractor_type(self, extractor_type):
         if extractor_type not in self.EXTRACTOR_TYPES:
             raise ValueError('Invalid extractor type')
-    
+
     def validate_action(self, rule_type, action):
         if action not in self.RULE_TO_ACTION_TYPES[rule_type]:
             raise ValueError('Invalid action for rule type')
