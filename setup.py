a59e4aed71ddf08519eeee9335bce6b84be20fff
yuenmichelle1 <michelle@zooniverse.org>
2022-06-16 16:52:52 -0500

    Update setup.py
    Co-authored-by: Cliff Johnson <cliff@zooniverse.org>

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name='panoptes_client',
     url='https://github.com/zooniverse/panoptes-python-client',
-    author='Adam McMaster',
-    author_email='adam@zooniverse.org',
+    author='Adam McMaster / Zooniverse',
+    author_email='contact@zooniverse.org',
     description=(
         'This package is the Python SDK for Panoptes, the platform behind the Zooniverse. This module is intended to allow programmatic management of projects, providing high level access to the API for common project management tasks.'
     ),
