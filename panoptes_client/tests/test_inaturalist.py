db39def8b76e77c315e931d65c92a443db4dbb03
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2022-11-04 16:07:53 -0500

    Add two broken tests

---
+++ panoptes_client/tests/test_inaturalist.py
@@ -0,0 +1,40 @@
+from __future__ import absolute_import, division, print_function
+
+import unittest
+import sys
+
+if sys.version_info <= (3, 0):
+    from mock import patch, Mock
+else:
+    from unittest.mock import patch, Mock
+
+from panoptes_client.inaturalist import Inaturalist
+
+class TestInaturalist(unittest.TestCase):
+    def test_inat_import(self):
+        with patch('panoptes_client.panoptes.Panoptes') as pc:
+            pc.client().post = Mock(return_value=200)
+            Inaturalist.inat_import(16462, 4)
+
+            pc.client().post.assert_called_with(
+                '/api/inaturalist/import',
+                json={
+                    'taxon_id': 16462,
+                    'subject_set_id': 4,
+                    'updated_since': None
+                },
+        )
+
+    def test_inat_import_updated_since(self):
+        with patch('panoptes_client.panoptes.Panoptes') as pc:
+            pc.client().json_request = Mock(return_value=200)
+            Inaturalist.inat_import(16462, 4, '2022-10-31')
+
+            pc.client().post.assert_called_with(
+                '/api/inaturalist/import',
+                json={
+                    'taxon_id': 16462,
+                    'subject_set_id': 4,
+                    'updated_since': '2022-10-31'
+                }
+            )
