6024758fcc31d18984a66b9ab5c575d0ba1e5773
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2022-11-10 15:08:59 -0600

    Return new class to init.py

--- panoptes_client/__init__.py
+++ panoptes_client/__init__.py
@@ -12,3 +12,4 @@ from panoptes_client.user import User
 from panoptes_client.workflow import Workflow
 from panoptes_client.subject_workflow_status import SubjectWorkflowStatus
 from panoptes_client.caesar import Caesar
+from panoptes_client.inaturalist import Inaturalist
