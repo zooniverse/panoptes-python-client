35834d69e2f9eda517eea4c866220a07a5df67ff
Oluwatoyosi Oyegoke <34948675+Tooyosi@users.noreply.github.com>
2024-06-11 19:27:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     ),
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='1.6.1',
+    version='1.6.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
