81b0a525a39fb83228a7997a0ec11bbe9cb4af97
tooyosi <oluwatoyosi.oyegoke@gmail.com>
2025-03-07 19:25:42 +0000

    use major version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     ),
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='1.6.3',
+    version='1.7.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
