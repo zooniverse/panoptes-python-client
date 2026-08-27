fe70f6846b15972d7334e4cc077501b6374edb13
yuenmichelle1 <michelle@zooniverse.org>
2026-08-27 16:53:21 -0500

    update tests for update_priority tests

--- panoptes_client/tests/test_subject.py
+++ panoptes_client/tests/test_subject.py
@@ -1,9 +1,10 @@
 import io
-import unittest
-from unittest.mock import patch, mock_open
-
-from panoptes_client.subject import Subject, UnknownMediaException
 import mimetypes
+import unittest
+from unittest.mock import patch, mock_open, MagicMock
+
+from panoptes_client.panoptes import ObjectNotSavedException
+from panoptes_client.subject import Subject, UnknownMediaException
 
 
 class TestSubject(unittest.TestCase):
@@ -39,7 +40,7 @@ class TestSubject(unittest.TestCase):
         self.assertIn("locations", self.subject.modified_attributes)
         mock_magic.from_buffer.assert_called_with(data, mime=True)
 
-    @patch.object(mimetypes, 'guess_type', return_value=("image/jpeg", None))
+    @patch("panoptes_client.subject.mimetypes.guess_type", return_value=("image/jpeg", None))
     def test_add_location_mimetypes_detection(self, mock_guess_type):
         import panoptes_client.subject as subject_module
         subject_module.MEDIA_TYPE_DETECTION = 'mimetypes'
@@ -57,3 +58,74 @@ class TestSubject(unittest.TestCase):
         fake_file = io.BytesIO(data)
         with self.assertRaises(UnknownMediaException):
             self.subject.add_location(fake_file, manual_mimetype="application/javascript")
+    
+    def test_update_priority_requires_saved_subject(self):
+        with self.assertRaises(ObjectNotSavedException):
+            self.subject.update_priority(1)
+
+    def test_update_priority_updates_priority_for_saved_subject(self):
+        self.subject.id = 123  
+        set_member_subject_mock = MagicMock()
+
+        with patch.object(self.subject, "save") as mock_save:
+            with patch(
+                "panoptes_client.subject.SetMemberSubject.where",
+                return_value=iter([set_member_subject_mock]),
+            ) as mock_where:
+                self.subject.update_priority(
+                    5,
+                    subject_set_id=456,
+                )
+
+        self.assertEqual(self.subject.metadata["priority"], 5)
+
+        mock_save.assert_called_once_with()
+        mock_where.assert_called_once_with(
+            subject_set_id=456,
+            subject_id=123,
+        )
+
+        self.assertEqual(set_member_subject_mock.priority, 5)
+        set_member_subject_mock.save.assert_called_once_with()
+    
+    def test_update_priority_updates_all_subject_sets(self):
+        self.subject.id = 123
+
+        subject_set_1 = MagicMock(id=456)
+        subject_set_2 = MagicMock(id=789)
+        set_member_subject_1 = MagicMock()
+        set_member_subject_2 = MagicMock()
+
+        with patch.object(self.subject, "save") as mock_save, \
+            patch(
+             "panoptes_client.panoptes.LinkResolver.__getattr__",
+             return_value=[subject_set_1, subject_set_2],
+            ),  \
+            patch(
+                "panoptes_client.subject.SetMemberSubject.where",
+                side_effect=[
+                    iter([set_member_subject_1]),
+                    iter([set_member_subject_2]),
+                ],
+            ) as mock_where:
+            self.subject.update_priority(5)
+
+        self.assertEqual(self.subject.metadata["priority"], 5)
+
+        mock_save.assert_called_once_with()
+
+        self.assertEqual(mock_where.call_count, 2)
+        mock_where.assert_any_call(
+            subject_set_id=456,
+            subject_id=123,
+        )
+        mock_where.assert_any_call(
+            subject_set_id=789,
+            subject_id=123,
+        )
+
+        self.assertEqual(set_member_subject_1.priority, 5)
+        set_member_subject_1.save.assert_called_once_with()
+
+        self.assertEqual(set_member_subject_2.priority, 5)
+        set_member_subject_2.save.assert_called_once_with()
